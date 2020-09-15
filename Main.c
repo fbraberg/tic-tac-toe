b69f0f0440b85352f99d82cb1e3c7bf11d76adea
Felix342 <fillebraberg@gmail.com>
2020-09-15 09:37:35 +0200

    Add: Now player and AI works

--- Main.c
+++ Main.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <conio.h>
 
 typedef struct t_Board
 {
-    /*   char row_A[3] = {' ', ' ', ' '};
-    char row_B[3] = {' ', ' ', ' '};
-    char row_C[3] = {' ', ' ', ' '};    */
-    
+
     char slots[3][3] = {{' ', ' ', ' '},
-                        {' ', ' ', ' '},
-                        {' ', ' ', ' '}};
+	{' ', ' ', ' '},
+	{' ', ' ', ' '}};
 
 } Board;
 Board board;
 
-void drawBoard()
+void clear_Screen() 
 {
+    for(int i = 0; i < 100; i ++) 
+    {
+	printf("\n");
+    }
+}
+
+void draw_Board()
+{
+    clear_Screen();
     printf("  A   B   C\n");
     printf("1 %c   %c   %c\n", board.slots[0][0], board.slots[0][1], board.slots[0][2]);
     printf("\n");
@@ -30,28 +38,27 @@ char* decode(char* move)
     int row;
     int col;
     static char res[2];
-    
+
     if (move[1] == '1')
-        row = 0;
+	row = 0;
     else if (move[1] == '2')
-        row = 1;
+	row = 1;
     else 
-        row = 2;
-    
+	row = 2;
+
     if (move[0] == 'A')
-        col = 0;
+	col = 0;
     else if (move[0] == 'B')
-        col = 1;
+	col = 1;
     else 
-        col = 2;
-    
+	col = 2;
+
     res[0] = row + '0';
     res[1] = col + '0';
     return res;
-    
+
 }
 
-// FIX
 bool empty(char move[])
 {
     int row = (int) move[0] - '0';
@@ -59,18 +66,28 @@ bool empty(char move[])
     return (board.slots[row][col] == ' ');
 }
 
-void placeSlot(const char* move)
+bool empty(const int row, const int col)
+{
+    return (board.slots[row][col] == ' ');
+}
+
+void place_Slot(const char* move, char symbol)
 {
     int row = (int) move[0] - '0';
     int col = (int) move[1] - '0';
-    board.slots[row][col] = 'O'; 
+    board.slots[row][col] = symbol;
 }
 
-void playerTurn()
+void place_Slot(const int row, const int col, char symbol)
+{
+    board.slots[row][col] = symbol;
+}
+
+void player_Turn()
 {
     char res[2];
     char* move;
-    
+
     printf("Pick a slot: ");
     scanf("%s", res);
     move = decode(res);
@@ -78,20 +95,91 @@ void playerTurn()
     while(!empty(move)) 
     {
 	printf("Occupied! Pick another slot:\n");
-        scanf("%s", res);
-        move = decode(res);
+	scanf("%s", res);
+	move = decode(res);
     }
 
-    placeSlot(move);
+    place_Slot(move, 'O');
+}
+
+void player_Wins()
+{
+    clear_Screen();
+    printf("You win!");
+    exit(0);
+}
+
+void AI_Wins() 
+{
+    clear_Screen();
+    printf("The AI wins");
+    exit(0);
 }
 
 void calc_Winner() 
 {
+    // Check every row
+    for(int row = 0; row < 3; row++)
+    {
+        if(board.slots[row][0] == board.slots[row][1] &&
+            board.slots[row][1] == board.slots[row][2]) 
+        {
+            if(board.slots[row][2] == 'O')
+                player_Wins();
+            else if(board.slots[row][2] == 'X')
+                AI_Wins();
+        }
+    }
     
+    // Check every column
+    for(int col = 0; col < 3; col++)
+    {
+        if(board.slots[0][col] == board.slots[1][col] &&
+            board.slots[1][col] == board.slots[2][col])
+        { 
+            if(board.slots[2][col] == 'O')
+                player_Wins();
+            else if(board.slots[2][col] == 'X') 
+                AI_Wins();    
+        }
+    }
+    
+    // Check the first diagonal
+    if(board.slots[0][0] == board.slots[1][1] &&
+       board.slots[1][1] == board.slots[2][2])
+    {
+        if(board.slots[2][2] == 'O')
+            player_Wins();
+        else if(board.slots[2][2] == 'X') 
+            AI_Wins();
+    }
+    
+    // Check the second diagonal
+    if(board.slots[0][2] == board.slots[1][1] &&
+       board.slots[1][1] == board.slots[2][0])
+    {
+        if(board.slots[2][0] == 'O')
+            player_Wins();
+        else if(board.slots[2][0] == 'X') 
+            AI_Wins();
+    }
 }
 
 
-void AI_turn();
+
+void AI_turn() 
+{
+    srand(time(0));
+    int row = rand()%3;
+    int col = rand()%3;
+
+    while(!empty(row, col)) 
+    {
+	row = rand()%3;
+	col = rand()%3;
+    }
+    place_Slot(row, col, 'X');
+}
 
 
 
@@ -99,13 +187,13 @@ void loop()
 {
     while(1) 
     {
-	drawBoard();
-	playerTurn();
-	drawBoard();
-	//calcWinner();
-	//AI turn
-	//Draw board
-	//Clac winner
+	draw_Board();
+	player_Turn();
+    draw_Board();
+	calc_Winner();
+	AI_turn();
+	draw_Board();
+	calc_Winner();
     }
 }
 
