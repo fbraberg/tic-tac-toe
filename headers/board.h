8a090ed4992316575852deafbd37972339e2901b
Felix342 <fillebraberg@gmail.com>
2020-09-30 19:50:26 +0200

    Started working on update_board function

--- headers/board.h
+++ headers/board.h
@@ -20,5 +20,6 @@ int empty_1(PBoard board, char move[]);
 int empty_2(PBoard board, const int row, const int col);
 void place_slot_1(PBoard board, const char* move, char symbol);
 void place_slot_2(PBoard board,const int row, const int col, char symbol);
+// TODO: Move calc_winner here
 
 #endif
--- src/board.c
+++ src/board.c
@@ -2,6 +2,51 @@
 
 static void t_update_board(PBoard board) {
     // This is where the magic happens
+    // Check every row                            // WORK FROM HERE
+    for(int row = 0; row < 3; row++)
+    {
+        if(board->slots[row][0] == board->slots[row][1] &&
+            board->slots[row][1] == board->slots[row][2])
+        {
+            if(board->slots[row][2] == PLAYER_SYMBOL)
+                player_wins();
+            else if(board->slots[row][2] == AI_SYMBOL)
+                ai_wins();
+        }
+    }
+
+    // Check every column
+    for(int col = 0; col < 3; col++)
+    {
+        if(board->slots[0][col] == board->slots[1][col] &&
+            board->slots[1][col] == board->slots[2][col])
+        {
+            if(board->slots[2][col] == PLAYER_SYMBOL)
+                player_wins();
+            else if(board->slots[2][col] == AI_SYMBOL)
+                ai_wins();
+        }
+    }
+
+    // Check the first diagonal
+    if(board->slots[0][0] == board->slots[1][1] &&
+       board->slots[1][1] == board->slots[2][2])
+    {
+        if(board->slots[2][2] == PLAYER_SYMBOL)
+            player_wins();
+        else if(board->slots[2][2] == AI_SYMBOL)
+            ai_wins();
+    }
+
+    // Check the second diagonal
+    if(board->slots[0][2] == board->slots[1][1] &&
+       board->slots[1][1] == board->slots[2][0])
+    {
+        if(board->slots[2][0] == PLAYER_SYMBOL)
+            player_wins();
+        else if(board->slots[2][0] == AI_SYMBOL)
+            ai_wins();
+    }
 }
 
 void init_board(PBoard board)
@@ -24,6 +69,7 @@ int empty_1(PBoard board, char move[])
 {
     int row = (int) move[0] - '0';
     int col = (int) move[1] - '0';
+    board->update_board(board);
     return (board->slots[row][col] == ' ');
 }
 
@@ -31,6 +77,7 @@ int empty_1(PBoard board, char move[])
 // Returns: 1 if move possible, 0 if not possible
 int empty_2(PBoard board, const int row, const int col)
 {
+    board->update_board(board);
     return (board->slots[row][col] == ' ');
 }
 
